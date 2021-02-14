c69433295c0590732582392cc1a4903f1307b3f7
Bastelschlumpf <44838536+Bastelschlumpf@users.noreply.github.com>
2021-02-14 10:51:17 +0100

    Copyright 2021

--- solarweather/Mqtt.h
+++ solarweather/Mqtt.h
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2018 SFini
+   Copyright (C) 2021 SFini
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
